d7b525d927241b96f6f40821df3d7b991749bb98
JohnCodes <93669966+ken0gul@users.noreply.github.com>
2023-03-15 11:51:40 -0400

    Create a function to add tasks

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoIosAddCircle } from "react-icons/io";
 import { TodoList } from "./TodoList";
+
+// Import firestore
+import { db } from "./firebase";
+import { addDoc, collection } from "firebase/firestore";
+
 const TodoApp = () => {
+  const [task, setTask] = useState([]);
+
+  // Add some items
+  const createTask = async (e) => {
+    e.preventDefault();
+
+    if (task === "") {
+      alert("Invalid input");
+      return;
+    }
+
+    await addDoc(collection(db, "todos"), {
+      task: task,
+      completed: false,
+    });
+
+    setTask("");
+  };
   return (
     <div className=" w-full bg-gray-300 m-auto rounded-md max-w-[500px] shadow-xl p-4">
       <h2>My To-Dos</h2>
-      <form>
-        <input type="text" placeholder="Add some task" />
+      <form onSubmit={createTask}>
+        <input
+          type="text"
+          placeholder="Add some task"
+          onChange={(e) => setTask(e.target.value)}
+          value={task}
+        />
         <button>
           <IoIosAddCircle size={25} />
         </button>
